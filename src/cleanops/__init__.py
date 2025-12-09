d9112cea8d67e03222fb9d20c98c01db0712f44c
Bulalo_Coder <javemelchorodchigue2006@gmail.com>
2025-12-09 19:05:13 +0800

    Update __init__.py

--- src/cleanops/__init__.py
+++ src/cleanops/__init__.py
@@ -3,7 +3,7 @@ from .data_preprocessor import DataInspector, DataCleaner, DataOrganizer
 from .data_output import DataExporter, ReportGenerator, DataOutput
 from .data_pipeline import DataPipeline
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 __all__ = [
     "DataGetter",
